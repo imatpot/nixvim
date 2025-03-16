c0fd99112f9e84257c3b79c61138458062c9355b
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-16 19:55:55 +0100

    feat: disable kak.nvim

--- configuration.nix
+++ configuration.nix
@@ -3,18 +3,5 @@
   utils,
   ...
 }: {
-  extraPlugins = with pkgs.vimPlugins; [
-    {
-      plugin = kak-nvim;
-      config = utils.luaToViml ''
-        require("kak").setup({
-          full = true,
-          which_key_integration = true,
-          experimental = {
-            rebind_visual_aiAI = true,
-          }
-        })
-      '';
-    }
-  ];
+  extraPlugins = with pkgs.vimPlugins; [];
 }
