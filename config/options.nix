231220dbe5462cc4b5e32e615bb2ea5e9add77f7
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-17 15:47:02 +0100

    feat: basic options

--- config/options.nix
+++ config/options.nix
@@ -3,9 +3,31 @@
     enableMan = false;
     clipboard.register = "unnamedplus";
 
+    viAlias = true;
+    vimAlias = true;
+
     opts = {
+      autoindent = true;
+      breakindent = true;
+      confirm = true;
+      cursorline = true;
+      expandtab = true;
+      filetype = "on";
+      ignorecase = true;
+      linebreak = true;
       number = true;
       relativenumber = true;
+      scrolloff = 10;
+      shiftwidth = 2;
+      showbreak = "↪";
+      smartcase = true;
+      smartindent = true;
+      smarttab = true;
+      splitbelow = true;
+      splitright = true;
+      swapfile = false;
+      undofile = true;
+      undolevels = 1000;
     };
   };
 }
