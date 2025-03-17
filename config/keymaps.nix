6bc64eec97d26cdf49ab3a09fb26aa004630a5d6
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-17 18:19:52 +0100

    feat: keymaps

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,7 +1,52 @@
 {...}: {
   config = {
     globals = {
-      mapleader = ",";
+      mapleader = " ";
     };
+
+    keymaps = [
+      {
+        key = "<";
+        action = "<<";
+        mode = "n";
+        options.desc = "Decrease indent";
+      }
+      {
+        key = ">";
+        action = ">>";
+        mode = "n";
+        options.desc = "Increase indent";
+      }
+      {
+        key = "<";
+        action = "<gv";
+        mode = "x";
+        options.desc = "Decrease indent";
+      }
+      {
+        key = ">";
+        action = ">gv";
+        mode = "x";
+        options.desc = "Increase indent";
+      }
+
+      {
+        key = "<leader>l";
+        action = "<CMD>set cursorline!<CR>";
+        options.desc = "Toggle cursorline";
+      }
+      {
+        key = "<leader>L";
+        action = "<CMD>set cursorcolumn!<CR>";
+        options.desc = "Toggle cursorcolumn";
+      }
+
+      {
+        key = " ";
+        action = "<nop>";
+        mode = "n";
+        options.desc = "Disable space motions";
+      }
+    ];
   };
 }
