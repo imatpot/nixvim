a3f0735a62f9964b141e7d28e76da92734cb3d0e
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-20 15:50:01 +0100

    fix: support update-nix-fetchgit

--- overlays/vim-plugins.nix
+++ overlays/vim-plugins.nix
@@ -1,36 +1,37 @@
+# nix run nixpkgs#update-nix-fetchgit --verbose overlays/*.nix
 self: super: {
   vimPlugins =
     super.vimPlugins
     // {
-      search = super.vimUtils.buildVimPlugin rec {
+      search = super.vimUtils.buildVimPlugin {
         name = "search.nvim";
         src = super.fetchFromGitHub {
           owner = "FabianWirth";
-          repo = name;
+          repo = "search.nvim";
           rev = "7b8f2315d031be73e14bc2d82386dfac15952614";
-          hash = "sha256-88rMEtHTk5jEQ00YleSr8x32Q3m0VFZdxSE2vQ+f0rM=";
+          sha256 = "1cyjkw7vsdi1qmfmcm5lg51zc7gkmgj9a62d8g29i4yks49crjpk";
         };
         doCheck = false;
       };
 
-      kak-nvim = super.vimUtils.buildVimPlugin rec {
+      kak-nvim = super.vimUtils.buildVimPlugin {
         name = "kak.nvim";
         src = super.fetchFromGitHub {
           owner = "mirlge";
-          repo = name;
+          repo = "kak.nvim";
           rev = "d4d55034232209d1476902ea004c48e01e06aa2a";
-          hash = "sha256-KkF6eh3LMOTDE87x/YB3E8pCNAptxgRkJ2v2e19gGEU=";
+          sha256 = "0i8qc1gppxkb4xj09ikd18s45jhkfy0gvwff2g1y8c6b3mx7lh9a";
         };
         doCheck = false;
       };
 
-      indentmini = super.vimUtils.buildVimPlugin rec {
+      indentmini = super.vimUtils.buildVimPlugin {
         name = "indentmini.nvim";
         src = super.fetchFromGitHub {
           owner = "nvimdev";
-          repo = name;
+          repo = "indentmini.nvim";
           rev = "59c2be5387e3a3308bb43f07e7e39fde0628bd4d";
-          hash = "sha256-RtNPlILvlEyIFfDK8NTq8LPZR5vIl6uBxeE3vftUS6g=";
+          sha256 = "1a2bakxvsdz1qn0sp5y8kd3xkczhxbag1jph2n44r57ghaa4zls6";
         };
         doCheck = false;
       };
