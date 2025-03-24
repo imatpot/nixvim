f20837a6caad7b58af7d99f76be5c17b54739216
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-24 23:59:57 +0100

    feat: better otter diagnostics

--- modules/otter.nix
+++ modules/otter.nix
@@ -6,6 +6,22 @@
 lib.utils.modules.mkSimple config true "otter" {
   plugins.otter = {
     enable = true;
-    settings.handle_leading_whitespace = true;
+    settings = {
+      handle_leading_whitespace = true;
+
+      lsp.diagnostic_update_events = [
+        "BufReadPost"
+        "BufWritePost"
+        "InsertLeave"
+      ];
+
+      buffers = {
+        preambles = {
+          lua = [
+            "---@diagnostic disable: undefined-global, trailing-space"
+          ];
+        };
+      };
+    };
   };
 }
