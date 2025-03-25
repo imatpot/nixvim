392ac57b14892ed71168ddc774cb440e7c5bdb29
Mladen Branković <mladen.brankovic@golog.ch>
2025-03-25 23:20:08 +0100

    feat: hide lua expr syntax errors

--- modules/otter.nix
+++ modules/otter.nix
@@ -18,10 +18,46 @@ lib.utils.modules.mkSimple config true "otter" {
       buffers = {
         preambles = {
           lua = [
-            "---@diagnostic disable: undefined-global, trailing-space"
+            # lua
+            "---@diagnostic disable: trailing-space, undefined-global"
           ];
         };
       };
     };
   };
+
+  extraConfigLuaPre = ''
+    local original_diagnostics_handler = vim.lsp.handlers["textDocument/publishDiagnostics"]
+
+    function OtterDiagnosticsFilter(err, result, ctx, config)
+      if not result or not result.diagnostics then return end
+
+      local bufnr = vim.uri_to_bufnr(result.uri)
+
+      if bufnr then
+        local bufinfo = vim.fn.getbufinfo(bufnr)[1];
+        local name = bufinfo.name;
+
+        if name:match("%.otter%.lua$") then
+          local filtered_diagnostics = {}
+          local ignored_diagnostics = {
+            "Unexpected <exp> .",
+            "<name> expected.",
+          }
+
+          for _, diagnostic in ipairs(result.diagnostics) do
+            if not vim.tbl_contains(ignored_diagnostics, diagnostic.message) then
+              table.insert(filtered_diagnostics, diagnostic)
+            end
+          end
+
+          result.diagnostics = filtered_diagnostics
+        end
+      end
+
+      original_diagnostics_handler(err, result, ctx, config)
+    end
+
+    vim.lsp.handlers["textDocument/publishDiagnostics"] = OtterDiagnosticsFilter
+  '';
 }
