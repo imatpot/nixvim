1265636007ae401c19f85e006c28f94e986e6d07
Mladen Branković <mladen.brankovic@golog.ch>
2025-04-03 23:37:34 +0200

    feat: hop

---
+++ modules/hop.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+lib.utils.modules.mkModule config true "hop" {
+  plugins.hop.enable = true;
+
+  keymaps = [
+    {
+      key = "f";
+      action = "<CMD>HopPattern<CR>";
+      options.desc = "Hop to pattern";
+    }
+    {
+      key = "F";
+      action = "<CMD>HopAnywhere<CR>";
+      options.desc = "Hop";
+    }
+  ];
+}
