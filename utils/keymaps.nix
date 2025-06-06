53e26ef46b1a7487cd121ffccb3b0e60a6d676d7
Mladen Branković <mladen.brankovic@golog.ch>
2025-06-06 20:04:32 +0200

    feat: mkBufferKeymap

--- utils/keymaps.nix
+++ utils/keymaps.nix
@@ -1,10 +1,16 @@
 {...}: rec {
+  mkKeymap' = mkKeymap "";
   mkKeymap = mode: key: action: desc: {
-    key = key;
-    action = action;
-    mode = mode;
+    inherit mode key action;
     options.desc = desc;
   };
 
-  mkKeymap' = mkKeymap "";
+  mkBufferKeymap' = mkBufferKeymap "";
+  mkBufferKeymap = mode: key: action: desc: {
+    inherit mode key action;
+    options = {
+      inherit desc;
+      buffer = true;
+    };
+  };
 }
