d74a1ad395edd06b6a9129b8b5659ff5299d7871
Mladen Branković <mladen.brankovic@golog.ch>
2025-06-06 20:11:21 +0200

    refactor: use mkBufferKeymap

--- modules/languages/dart.nix
+++ modules/languages/dart.nix
@@ -46,156 +46,32 @@ lib.utils.modules.mkLanguage' config "dart"
           runtime! -buffer ftplugin/yaml.vim
         '';
 
-      keymaps = [
-        {
-          key = "<localleader>a";
-          action = "<CMD>PubspecAssistAddPackage<CR>";
-          options = {
-            desc = "Add package";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>A";
-          action = "<CMD>PubspecAssistAddDevPackage<CR>";
-          options = {
-            desc = "Add dev package";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>v";
-          action = "<CMD>PubspecAssistPickVersion<CR>";
-          options = {
-            desc = "Pick version";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>g";
-          action = "<CMD>FlutterPubGet<CR>";
-          options = {
-            desc = "Pub get";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>u";
-          action = "<CMD>FlutterPubUpgrade<CR>";
-          options = {
-            desc = "Pub upgrade";
-            buffer = true;
-          };
-        }
+      keymaps = with lib.utils.keymaps; [
+        (mkBufferKeymap' "<localleader>a" "<CMD>PubspecAssistAddPackage<CR>" "Add package")
+        (mkBufferKeymap' "<localleader>A" "<CMD>PubspecAssistAddDevPackage<CR>" "Add dev package")
+        (mkBufferKeymap' "<localleader>v" "<CMD>PubspecAssistPickVersion<CR>" "Pick version")
+        (mkBufferKeymap' "<localleader>g" "<CMD>FlutterPubGet<CR>" "Pub get")
+        (mkBufferKeymap' "<localleader>u" "<CMD>FlutterPubUpgrade<CR>" "Pub upgrade")
       ];
     };
 
     "ftplugin/dart.lua" = {
-      keymaps = [
-        {
-          key = "<leader>ds";
-          action = "<CMD>lua if require('dap').session() ~= nil then require('dap').continue() else vim.cmd('FlutterRun') end<CR>";
-          options = {
-            desc = "Start / Continue";
-            buffer = true;
-          };
-        }
-        {
-          key = "<leader>da";
-          action = "<CMD>FlutterAttach<CR>";
-          options = {
-            desc = "Attach";
-            buffer = true;
-          };
-        }
-        {
-          key = "<leader>dr";
-          action = "<CMD>FlutterReload<CR>";
-          options = {
-            desc = "Hot-Relod";
-            buffer = true;
-          };
-        }
-        {
-          key = "<leader>dR";
-          action = "<CMD>FlutterRestart<CR>";
-          options = {
-            desc = "Hot-Restart";
-            buffer = true;
-          };
-        }
-        {
-          key = "<leader>dQ";
-          action = "<CMD>FlutterDetach<CR>";
-          options = {
-            desc = "Detach";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>e";
-          action = "<CMD>FlutterEmulators<CR>";
-          options = {
-            desc = "Emulators";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>d";
-          action = "<CMD>FlutterDevices<CR>";
-          options = {
-            desc = "Devices";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>o";
-          action = "<CMD>FlutterOutlineToggle<CR>";
-          options = {
-            desc = "Outline";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>i";
-          action = "<CMD>FlutterDevTools<CR>";
-          options = {
-            desc = "Dev-Tools";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>l";
-          action = "<CMD>FlutterLogToggle<CR>";
-          options = {
-            desc = "Toggle logs";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>c";
-          action = "<CMD>FlutterLogClear<CR>";
-          options = {
-            desc = "Clear logs";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>g";
-          action = "<CMD>FlutterPubGet<CR>";
-          options = {
-            desc = "Pub get";
-            buffer = true;
-          };
-        }
-        {
-          key = "<localleader>u";
-          action = "<CMD>FlutterPubUpgrade<CR>";
-          options = {
-            desc = "Pub upgrade";
-            buffer = true;
-          };
-        }
+      keymaps = with lib.utils.keymaps; [
+        (mkBufferKeymap' "<leader>ds" "<CMD>lua if require('dap').session() ~= nil then require('dap').continue() else vim.cmd('FlutterRun') end<CR>" "Start / Continue")
+
+        (mkBufferKeymap' "<leader>da" "<CMD>FlutterAttach<CR>" "Attach")
+        (mkBufferKeymap' "<leader>dr" "<CMD>FlutterReload<CR>" "Hot-Relod")
+        (mkBufferKeymap' "<leader>dR" "<CMD>FlutterRestart<CR>" "Hot-Restart")
+        (mkBufferKeymap' "<leader>dQ" "<CMD>FlutterDetach<CR>" "Detach")
+
+        (mkBufferKeymap' "<localleader>e" "<CMD>FlutterEmulators<CR>" "Emulators")
+        (mkBufferKeymap' "<localleader>d" "<CMD>FlutterDevices<CR>" "Devices")
+        (mkBufferKeymap' "<localleader>o" "<CMD>FlutterOutlineToggle<CR>" "Outline")
+        (mkBufferKeymap' "<localleader>i" "<CMD>FlutterDevTools<CR>" "Dev-Tools")
+        (mkBufferKeymap' "<localleader>l" "<CMD>FlutterLogToggle<CR>" "Toggle logs")
+        (mkBufferKeymap' "<localleader>c" "<CMD>FlutterLogClear<CR>" "Clear logs")
+        (mkBufferKeymap' "<localleader>g" "<CMD>FlutterPubGet<CR>" "Pub get")
+        (mkBufferKeymap' "<localleader>u" "<CMD>FlutterPubUpgrade<CR>" "Pub upgrade")
       ];
     };
   };
