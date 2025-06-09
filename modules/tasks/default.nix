a0b32c10981a0a3cc206a827570374f6373b4836
Mladen Branković <mladen.brankovic@golog.ch>
2025-06-09 20:01:57 +0200

    refactor: merge nx and task runner

--- modules/tasks/default.nix
+++ modules/tasks/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 lib.utils.modules.mkModule config true "tasks" {
@@ -12,6 +13,27 @@ lib.utils.modules.mkModule config true "tasks" {
   keymaps = with lib.utils.keymaps; [
     (mkKeymap' "<leader>xx" "<CMD>OverseerToggle<CR>" "Open task runner")
     (mkKeymap' "<leader>xs" "<CMD>OverseerRun<CR>" "Run task")
-    (mkKeymap' "<leader>xS" "<CMD>lua OverseerRunAndOpen()<CR>" "Run task")
+    (mkKeymap' "<leader>xS" "<CMD>lua OverseerRunAndOpen()<CR>" "Run task and open task runner")
+
+    (mkKeymap' "<leader>xns" "<CMD>Telescope nx actions<CR>" "Nx run")
+    (mkKeymap' "<leader>xna" "<CMD>Telescope nx affected<CR>" "Nx affected")
+    (mkKeymap' "<leader>xng" "<CMD>Telescope nx generators<CR>" "Nx generate")
+    (mkKeymap' "<leader>xnGe" "<CMD>Telescope nx external_generators<CR>" "Nx workspace generators")
+    (mkKeymap' "<leader>xnGw" "<CMD>Telescope nx workspace_generators<CR>" "Nx workspace generators")
+    (mkKeymap' "<leader>xnm" "<CMD>Telescope nx run_many<CR>" "Nx run many")
+  ];
+
+  extraPlugins = with pkgs.vimPlugins; [
+    {
+      plugin = nx-nvim;
+      config =
+        lib.utils.viml.fromLua
+        # lua
+        ''
+          require("nx").setup({
+            nx_cmd_root = "npx nx",
+          })
+        '';
+    }
   ];
 }
