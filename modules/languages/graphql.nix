87146f0adaa2dbb85ca06916c589d7a7e0414d4d
Mladen Branković <mladen.brankovic@golog.ch>
2025-07-20 01:04:34 +0200

    feat: graphql

---
+++ modules/languages/graphql.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+lib.utils.modules.mkLanguage' config "graphql" {
+  plugins = {
+    lsp.servers.graphql = {
+      enable = true;
+      package = pkgs.graphql-language-service-cli;
+    };
+
+    conform-nvim.settings = {
+      formatters.prettierd.command = lib.getExe pkgs.prettierd;
+      formatters_by_ft.graphql = ["prettierd"];
+    };
+  };
+}
