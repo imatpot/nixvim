260b9d1e483cd0b5ffc400814cbe8204c8f70ef1
Mladen Branković <mladen.brankovic@golog.ch>
2025-08-02 20:37:17 +0200

    refactor: improve telescope

--- modules/telescope.nix
+++ modules/telescope.nix
@@ -13,72 +13,57 @@ lib.utils.modules.mkModule' config true "telescope" {
       mappings.i."<esc>" = helpers.mkRaw "require('telescope.actions').close";
 
       file_ignore_patterns = [
+        "^%.dart_tool/"
+        "^%.git/"
+        "^%.venv/"
         "^node_modules/"
         "^venv/"
-        "^.venv/"
       ];
+
+      vimgrep_arguments = [
+        "rg"
+        "-uuu"
+        "--hidden"
+        "--color=never"
+        "--no-heading"
+        "--with-filename"
+        "--line-number"
+        "--column"
+        "--smart-case"
+        "--trim"
+      ];
+
+      pickers = {
+        find_files.find_command = [
+          "rg"
+          "-uuu"
+          "--files"
+          "--hidden"
+          "--glob"
+          "!**/.git/*"
+        ];
+      };
     };
 
     extensions = {
-      ui-select.enable = true;
-      file-browser.enable = true;
-      fzy-native = {
+      ui-select = {
         enable = true;
         settings = {
-          override_file_sorter = true;
-          override_generic_sorter = true;
+          __unkeyed_1 = helpers.mkRaw "require('telescope.themes').get_cursor()";
         };
       };
     };
   };
 
-  extraPlugins = with pkgs.vimPlugins; [
-    {
-      plugin = search;
-      config =
-        lib.utils.viml.fromLua
-        # lua
-        ''
-          local builtin = require('telescope.builtin')
-          require('search').setup({
-            initial_tab = 1,
-            tabs = {
-              { name = "Files", tele_func = builtin.find_files },
-              { name = "Grep", tele_func = builtin.live_grep },
-              { name = "Buffers", tele_func = builtin.buffers },
-            }
-          })
-        '';
-    }
+  keymaps = with lib.utils.keymaps; [
+    (mkKeymap' "<leader>p" "<CMD>Telescope find_files hidden=true<CR>" "Search files")
+    (mkKeymap' "<leader>/" "<CMD>Telescope live_grep<CR>" "Grep files")
+
+    (mkKeymap' "gi" "<CMD>Telescope lsp_implementations<CR>" "Go to implementations")
+    (mkKeymap' "gd" "<CMD>Telescope lsp_definitions<CR>" "Go to definitions")
+    (mkKeymap' "gr" "<CMD>Telescope lsp_references<CR>" "Go to references")
+    (mkKeymap' "gt" "<CMD>Telescope lsp_type_definitions<CR>" "Go to type definitions")
   ];
-
-  keymaps = let
-    search = "lua require('search')";
-    teleOpts =
-      # lua
-      ''
-        tele_opts = {
-          no_ignore = true,
-          no_ignore_parent = true,
-          hidden = true,
-          use_regex = true,
-          file_ignore_patterns = {
-            "^.git/",
-            "^node_modules/",
-            "^.dart_tool/",
-          },
-        }
-      '';
-  in
-    with lib.utils.keymaps; [
-      (mkKeymap' "<leader>p" "<CMD>${search}.open({ tab_name = 'Files', ${teleOpts} })<CR>" "Search files")
-      (mkKeymap' "<leader>/" "<CMD>${search}.open({ tab_name = 'Grep', ${teleOpts} })<CR>" "Grep files")
-
-      (mkKeymap' "gi" "<CMD>Telescope lsp_implementations<CR>" "Go to implementations")
-      (mkKeymap' "gd" "<CMD>Telescope lsp_definitions<CR>" "Go to definitions")
-      (mkKeymap' "gr" "<CMD>Telescope lsp_references<CR>" "Go to references")
-      (mkKeymap' "gt" "<CMD>Telescope lsp_type_definitions<CR>" "Go to type definitions")
-    ];
 
   extraPackages = with pkgs; [
     ripgrep
