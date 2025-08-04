e61134e0766aa7450fa0df19481e8ab82ee16e76
Mladen Branković <mladen.brankovic@golog.ch>
2025-08-04 18:42:24 +0200

    feat: mkdir -p on save

--- config/autocommands.nix
+++ config/autocommands.nix
@@ -1,20 +1,30 @@
-{...}: {
+{helpers, ...}: {
   config = {
     autoCmd = [
       {
         event = ["CmdwinEnter"];
-        pattern = ["*"];
         command = "q";
       }
       {
         event = ["InsertEnter" "InsertChange"];
-        pattern = ["*"];
         command = "call ForbidReplace()";
       }
       {
         event = ["FocusGained" "CursorHold"];
-        pattern = ["*"];
         command = "checktime";
+      }
+      {
+        event = ["BufWritePre"];
+        callback = helpers.mkRaw ''
+          function (event)
+            if event.match:match("^%w%w+:[\\/][\\/]") then
+              return
+            end
+
+            local file = vim.uv.fs_realpath(event.match) or event.match
+            vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+          end
+        '';
       }
     ];
 
