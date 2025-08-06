d3b79445e986fc9afa8a8d55f24b6d7bcb5decd4
Mladen Branković <mladen.brankovic@golog.ch>
2025-08-06 19:49:16 +0200

    feat: colorizer

---
+++ modules/colors.nix
@@ -0,0 +1,48 @@
+{
+  config,
+  lib,
+  helpers,
+  ...
+}:
+lib.utils.modules.mkModule' config true "colors" {
+  plugins = {
+    colorizer = {
+      enable = true;
+      settings = {
+        user_default_options = {
+          mode = "virtualtext";
+          virtualtext = "";
+          virtualtext_inline = helpers.mkRaw "'before'";
+
+          names = true;
+          names_opts = {
+            lowercase = true;
+            camelcase = true;
+            uppercase = true;
+          };
+
+          css = true;
+          css_fn = true;
+          sass = {
+            enable = true;
+            parsers = ["css"];
+          };
+
+          tailwind = true;
+          xterm = true;
+        };
+      };
+    };
+
+    ccc = {
+      enable = true;
+      settings.highlighter.auto_enable = false;
+    };
+  };
+
+  keymaps = with lib.utils.keymaps; [
+    (mkKeymap' "<leader>Ch" "<CMD>ColorizerToggle<CR>" "Toggle colors highlights")
+    (mkKeymap' "<leader>Cc" "<CMD>CccConvert<CR>" "Convert color")
+    (mkKeymap' "<leader>C/" "<CMD>CccPick<CR>" "Pick color")
+  ];
+}
